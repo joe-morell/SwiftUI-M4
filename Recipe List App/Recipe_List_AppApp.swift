20dd50a3a6bc7facb23327bc3625d7126b6df4f0
Joseph Morell <josephtmorell@gmail.com>
2021-07-22 17:27:52 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Joe Morell on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
